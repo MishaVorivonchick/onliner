b0ed2a08bc1a756afdfb9d0e37f5b0b34d4f323c
misha <varik-1986-varik@mail.ru>
2023-06-28 21:38:51 +0300

    Initial commit

---
+++ locator/locator.py
@@ -0,0 +1,12 @@
+
+
+
+CATALOG_SEARCH = "//*[@id='fast-search']/form/input[1]"
+CATALOG_BUTTON = "//*[@id='container']/div/div/header/div[2]/div/nav/ul[1]/li[1]/a[2]/span"
+EMAIL = "//div[@class='auth-form__row auth-form__row_condensed-alter']//input"
+PASSWORD = "//input[@type='password']"
+SUBMIT = '//*[@id="auth-container"]/div/div[2]/div/form/div[3]'
+CURRENCY_BUYING = "//span[@class= '_u js-currency-amount']"
+SECTION_ELECTRONICS = "//li[@data-id = 1]"
+USD_SALE = "//table[@class='b-currency-table__best'][1]//td[3]//b"
+USD_BUYING = "//table[@class='b-currency-table__best'][1]//td[2]//b"
---
+++ page/BasePage.py
@@ -0,0 +1,9 @@
+import allure
+import allure
+from selenium.common import TimeoutException
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+class BasePage:
